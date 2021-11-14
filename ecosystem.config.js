bd0bae1b35f8eb1ec2bd4f2a99ed66a64e3560b9
danylaksono <danylaksono@ugm.ac.id>
2021-11-15 04:04:24 +0700

    change default port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,6 @@ module.exports = {
   apps : [{
     name   : "Sitaru Web",
     script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
-    args: "serve"
+    args: "serve --port 8000"
   }]
 }
